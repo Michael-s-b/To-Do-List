309aa0cf4aea4dadbe0857a290b5fa31f9851baf
michael-s-b <michelsilva098@gmail.com>
2022-12-16 21:05:02 -0300

    overral ejs and add css

--- dist/app.js
+++ dist/app.js
@@ -4,24 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.static(path_1.default.join(__dirname, "/public")));
 app.set("view engine", "ejs"); //setting the view engine of the application to ejs.
 app.set("views", path_1.default.join(__dirname, "/views")); // setting the app to look for the views dir on the correct dir
-var Weekdays;
-(function (Weekdays) {
-    Weekdays[Weekdays["Monday"] = 1] = "Monday";
-    Weekdays[Weekdays["Tuesday"] = 2] = "Tuesday";
-    Weekdays[Weekdays["Wednesday"] = 3] = "Wednesday";
-    Weekdays[Weekdays["Thursday"] = 4] = "Thursday";
-    Weekdays[Weekdays["Friday"] = 5] = "Friday";
-    Weekdays[Weekdays["Saturday"] = 6] = "Saturday";
-    Weekdays[Weekdays["Sunday"] = 7] = "Sunday";
-})(Weekdays || (Weekdays = {}));
+let listOfItems = [];
 app.get("/", (req, res) => {
     let date = new Date();
-    let dayOfTheWeek = Weekdays[date.getDay()];
-    res.render("list", { dayOfTheWeek: dayOfTheWeek });
+    let options = {
+        //options to parse the Date returned by the function toLocaleDateString
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
+    let parsedDate = date.toLocaleDateString("en-US", options);
+    res.render("list", { parsedDate: parsedDate, listOfItems: listOfItems });
+});
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
+    listOfItems.push(item);
+    res.redirect("/");
 });
 app.listen(3000, () => {
     console.log("Listening on port 3000...");
--- src/app.ts
+++ src/app.ts
@@ -4,24 +4,30 @@ import ejs from "ejs";
 import path from "path";
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/public")));
 app.set("view engine", "ejs"); //setting the view engine of the application to ejs.
 app.set("views", path.join(__dirname, "/views")); // setting the app to look for the views dir on the correct dir
 
-enum Weekdays {
-	"Monday" = 1,
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday",
-	"Sunday",
-}
+let listOfItems: string[] = [];
 
 app.get("/", (req, res) => {
 	let date = new Date();
-	let dayOfTheWeek = Weekdays[date.getDay()];
-	res.render("list", { dayOfTheWeek: dayOfTheWeek });
+	let options: Intl.DateTimeFormatOptions = {
+		//options to parse the Date returned by the function toLocaleDateString
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+	};
+	let parsedDate = date.toLocaleDateString("en-US", options);
+
+	res.render("list", { parsedDate: parsedDate, listOfItems: listOfItems });
+});
+
+app.post("/", (req, res) => {
+	let item: string = req.body.newItem;
+	listOfItems.push(item);
+	res.redirect("/");
 });
 
 app.listen(3000, () => {
